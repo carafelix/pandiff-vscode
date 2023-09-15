751f58e5a6a1a15c086851b34882d8bb4da3ded9
carafelix <heroprotagonist32@gmail.com>
2023-09-15 16:53:58 -0300

    async get files path

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,25 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { runPandiffAndGetHTML } from './getDif';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+	let orange = vscode.window.createOutputChannel("Orange");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pandiff-vscode" is now active!');
+	let getFiles = vscode.commands.registerCommand('pandiff-vscode.difs', async function() {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pandiff-vscode.difs', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Pandiff-VScode!');
+		let files = await vscode.workspace.findFiles('{**/*.epub,**/*.odt,**/*.txt,**/*.md,**/*.html,**/*.docx}','**/node_modules/**');
+
+			orange.append(files.constructor())
+			orange.show();
+		
+		vscode.window.showInformationMessage(`${files}`)
+
+		vscode.window.showQuickPick(['asd'])
+
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(getFiles);
 }
 
 // This method is called when your extension is deactivated
