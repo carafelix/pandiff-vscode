a21f2aee2a2d5b232b7fc06c1a2353ea383b41c7
carafelix <heroprotagonist32@gmail.com>
2023-09-15 22:43:47 -0300

    big

--- src/extension.ts
+++ src/extension.ts
@@ -50,11 +50,13 @@ export async function activate(context: vscode.ExtensionContext) {
 					<meta name="viewport" content="width=device-width, initial-scale=1.0">
 					<title>Files Diff</title>
 				</head>
-				<body align="center">
+				<body>
 					<style>
 						${style}
 					</style>
+					<div align="center">
 						${content}
+					</div>
 				</body>
 				</html>`;
 			}
