73f5199f8e1fc3f8e7474983a09e82ce70f36d56
carafelix <heroprotagonist32@gmail.com>
2023-09-17 13:55:53 -0300

    improve read

--- src/test/ptest.ts
+++ src/test/ptest.ts
@@ -1,5 +1,7 @@
 import pandiff = require("pandiff");
 
+const testFile = ['/home/martincito/repos/pandiff-vscode/testFiles/gateway.epub','/home/martincito/repos/pandiff-vscode/testFiles/gatewayed.epub']
+
 async function supa(str1:string,str2:string){
     const result = await pandiff(str1,str2, {
         to:'html',
@@ -9,5 +11,5 @@ async function supa(str1:string,str2:string){
     
     return result
 }
-supa('/home/martincito/repos/pandiff-vscode/testFiles/gateway.epub','/home/martincito/repos/pandiff-vscode/testFiles/gatewayed.epub')
+supa(testFile[0],testFile[1])
 
