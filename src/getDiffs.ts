fea1c993b1f9499cbe0174ff0b507254122190ea
carafelix <heroprotagonist32@gmail.com>
2023-09-17 12:33:10 -0300

    removed the need for exec on selected files

--- src/getDiffs.ts
+++ src/getDiffs.ts
@@ -1,25 +1,18 @@
+import pandiff = require("pandiff");
+import * as vscode from "vscode";
+
 const exec = require('child_process').exec;
 
     export async function runPandiffAndGetHTML(f1Path: string, f2Path: string): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const command = `pandiff ${f1Path} ${f2Path} --to=html`;
-
-            exec(command, (error:Error, stdout:string, stderr:string) => {
-                if (error) {
-                    console.error(`Error running pandiff: ${error.message}`);
-                    reject(error);
-                    return;
-                }
-        
-                if (stderr) {
-                    console.error(`pandiff produced an error message: ${stderr}`);
-                    reject(new Error(stderr));
-                    return;
-                }
-        
-                resolve(stdout);
-            });
+        const result = await pandiff(f1Path,f2Path,{
+            to: 'html',
+            files: true
         });
+
+        if(!result){
+            vscode.window.showErrorMessage('Result is null')
+            throw new Error('Result is null')
+        } else return result
     }
 
     export async function getGitDiffs(hash:string,filename:string,parentFolder:string):Promise<string> {
