a32be33a02419921d9bc6a9a3704be2a0082ff88
Hero Protagonist <heroprotagonist32@gmail.com>
2024-01-12 20:12:28 -0300

    add check for file is part of a git repo

---
+++ src/checksRequirements.ts
@@ -0,0 +1,39 @@
+import { exec } from 'child_process';
+import * as vscode from 'vscode';
+import * as path from 'path'
+import { getCommitsFullInfo } from './fileRelated_F'
+import { simpleGit, SimpleGit, CleanOptions } from 'simple-git';
+const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE);
+
+
+export async function isPandocInstalled():Promise<boolean>{
+    return new Promise((resolve,reject)=>{
+      exec('pandoc -v', (err,stdout)=>{
+        if(err){
+          reject(err)
+        } else if (stdout) resolve(stdout);
+      })
+    }).then((response)=>{
+      return true
+    })
+      .catch((err)=>{
+		    vscode.window.showErrorMessage('Pandoc is missing. Visit https://pandoc.org/installing.html')
+        return false
+      })
+}
+
+export async function isGitRepo(filePath : string){
+  const parentPath = path.dirname(filePath) + '/'
+  const execGitPromise = await new Promise((resolve,reject)=>{
+    exec(`cd ${parentPath} && git log`, (err,stdout)=>{
+      if(err){
+        reject(err)
+      } else if (stdout) resolve(stdout);
+    })
+  }).then((sucess)=>{
+    return true
+  }).catch((err)=>{
+      return false
+    })
+    return execGitPromise
+}
